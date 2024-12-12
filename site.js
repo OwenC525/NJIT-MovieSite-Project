8cda0aa142f805af8333432c6904ff73ac688a03
OwenC525 <owencc8@gmail.com>
2024-12-12 11:36:48 -0500

    Step 2 and 3: Added an app-binding and created a title, owner, and github variables in site.js

--- site.js
+++ site.js
@@ -34,7 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
+            title: "IMDB + Owen's Top 8 Movies",
+            owner: "Owen C",
+            github: "https://github.com/OwenC525/NJIT-MovieSite-Project"
       }
     },
       methods: {
